3070b2ec15efbe6abea4a8ad8ca445e024c17561
Steffen May <steffen.may@posteo.de>
2026-08-15 14:16:01 +0200

    lbenv: use sysprof.dev for sysprof-capture-4.pc

--- flake.nix
+++ flake.nix
@@ -205,7 +205,7 @@
               dejavu_fonts liberation_ttf cacert
               patchelf glslang
               libdrm.dev vulkan-headers vulkan-loader.dev
-              libGL.dev libpulseaudio.dev glib.dev sysprof
+              libGL.dev libpulseaudio.dev glib.dev sysprof.dev
               qt6Packages.qtmultimedia qt6Packages.qtpositioning qt6Packages.qtwayland
             ]);
 
